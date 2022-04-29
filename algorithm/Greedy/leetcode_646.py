98ed14d4549e2fb64b57005615f5283ceee2de92
kangheeleeai <91243525+kangheeleeai@users.noreply.github.com>
2022-04-29 23:27:57 +0900

    Create leetcode_646.py

---
+++ algorithm/Greedy/leetcode_646.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/maximum-length-of-pair-chain/
+
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        pairs = sorted(pairs, key = lambda x: x[0])
+        n = len(pairs)
+        answer = [pairs[0]]
+        
+        for i in range(1, n):
+            if answer[-1][1] >= pairs[i][0]:
+                if answer[-1][1] >= pairs[i][1]:
+                    answer.pop()
+                    answer.append(pairs[i])
+            else:
+                answer.append(pairs[i])
+                
+        return len(answer)
