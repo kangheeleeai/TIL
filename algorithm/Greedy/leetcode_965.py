aa8c41d70966c98142468a54a306accc3a192504
kangheeleeai <91243525+kangheeleeai@users.noreply.github.com>
2022-05-07 13:36:59 +0900

    Create leetcode_965.py

---
+++ algorithm/Greedy/leetcode_965.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/univalued-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node, val):
+            if not node:
+                return True
+            if node.val != val:
+                return False
+            
+            return dfs(node.left, val) and dfs(node.right, val)
+            
+        return dfs(root, root.val)
