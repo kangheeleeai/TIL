8de6400adbe0ddb3a459bca8abe106553a334b9e
kangheeleeai <91243525+kangheeleeai@users.noreply.github.com>
2022-05-27 23:40:06 +0900

    Create leetcode_118.py

---
+++ algorithm/DP/leetcode_118.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/pascals-triangle/
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        answer=list()
+        for idx, _ in enumerate(range(numRows)):
+            if idx==0:
+                answer.append([1])
+            else:
+                row=list()
+                
+                for i in range(len(answer[idx-1])):
+                    if i==0:
+                        row.append(1)
+                    else:
+                        row.append(answer[idx-1][i-1]+answer[idx-1][i])
+                row.append(1)
+                answer.append(row)
+        return answer
