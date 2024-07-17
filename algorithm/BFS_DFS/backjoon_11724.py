be9bdda403d2d329b2e52a017ac8ed70262c31ae
kangheeleeai <91243525+kangheeleeai@users.noreply.github.com>
2024-07-17 14:38:44 +0900

    Create backjoon_11724.py

---
+++ algorithm/BFS_DFS/backjoon_11724.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/11724
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for i in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)  
+
+count = 0
+visited = [False] * (N+1)
+for i in range(1, N+1):
+    if not visited[i]:
+        dfs(graph, i, visited)
+        count += 1
+
+print(count)
